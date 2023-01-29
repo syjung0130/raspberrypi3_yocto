481d34578b5161fdbf518371855268232c0bcc04
SOONYEONG JUNG <soonyoung87@gmail.com>
2023-01-29 20:03:35 +0900

    update docker.

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+<<<<<<< HEAD
+docker run -it --volume="$PWD/..:/home/rpi" rpi:imx-1
+=======
 docker run -it --volume="$PWD/..:/workdir/rpi" rpi:imx-1
+>>>>>>> f7a0a17a22129a3021a690fb6491d14cfbaa8fff
